7e670dd330a6c8e260ecba6e907aea588d4a7445
bloss0mx <bloss0mx@163.com>
2019-02-20 02:48:19 +0800

    fix toJS函数类型检查bug

--- vph/diff/objectDiff.ts
+++ vph/diff/objectDiff.ts
@@ -1,8 +1,15 @@
+import { } from '../DataUnit';
+
+interface chged {
+  name: number | string;
+  item: any;
+}
+
 export default function rkeys(oldObj, newObj) {
   const keys = new Set([...Object.keys(oldObj), ...Object.keys(newObj)]);
-  const add = [];
-  const rm = [];
-  const update = [];
+  const add: Array<chged> = [];
+  const rm: Array<string | number> = [];
+  const update: Array<string | number> = [];
   for (let i of keys) {
     if (!oldObj.hasOwnProperty(i)) {
       add.push({ name: i, item: newObj[i] });
