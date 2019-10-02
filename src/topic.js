9badee22b9c3a650fe6b843b53a18192afb2afe2
bloss0mx <bloss0mx@163.com>
2019-10-03 01:37:23 +0800

    feat: 暴露了重复渲染,没有最小化的问题

--- src/topic.js
+++ src/topic.js
@@ -15,76 +15,94 @@ export default Component({
     ifyo: false,
     subArr: [],
     diff: '',
+    table:[],
   },
   whenMount() {
-    setTimeout(() => {
+    setInterval(() => {
       this.setState(state => {
-        state.diff = 'haha';
+        state.table = [];
+        for(let i = 0;i<3;i++){
+          const tmp = [];
+          for(let j = 0;j<3;j++){
+            const tmp2 = [];
+            for(let k = 0;k<3;k++){
+              tmp2.push(Math.floor(10*Math.random()));
+            }
+            tmp.push(tmp2);
+          }
+          state.table.push(tmp);
+        }
         return state;
-      });
-    }, 1000);
-    setTimeout(() => {
-      this.setState(state => {
-        state.arr = [...state.arr];
-        state.arr.splice(1, 0, {
-          name: 'b'
-        }, {
-          name: 'd'
-        });
-        return state;
-      });
-    }, 2000);
-    setTimeout(() => {
-      this.setState(state => {
-        state.arr = [{
-          name: '-1'
-        }, ...state.arr];
-        return state;
-      });
-    }, 3000);
-    setTimeout(() => {
-      this.setState(state => {
-        state.preArr = [...state.preArr, {
-          name: '-2'
-        }];
-        return state;
-      });
-    }, 4000);
-    setTimeout(() => {
-      this.setState(state => {
-        state.ifyo = true;
-        return state;
-      });
-    }, 5000);
-    setTimeout(() => {
-      this.setState(state => {
-        state.subArr = [...state.subArr, {
-          name: 'e'
-        }];
-        return state;
-      });
-    }, 6000);
-    setTimeout(() => {
-      this.setState(state => {
-        state.arr = [];
-        return state;
-      });
-    }, 7000);
-    setTimeout(() => {
-      this.setState(state => {
-        return {
-          preArr: [],
-          arr: ['a', 'c'].map(item => ({
-            name: item
-          })),
-          ifyo: false,
-          subArr: [],
-          diff: '',
-        };
       })
-    }, 8000);
-    setTimeout(() => {
-      console.dir(this.storeKeeper.store.toJS());
-    }, 9000);
+    }, 2000);
+    // setTimeout(() => {
+    //   this.setState(state => {
+    //     state.diff = 'haha';
+    //     return state;
+    //   });
+    // }, 1000);
+    // setTimeout(() => {
+    //   this.setState(state => {
+    //     state.arr = [...state.arr];
+    //     state.arr.splice(1, 0, {
+    //       name: 'b'
+    //     }, {
+    //       name: 'd'
+    //     });
+    //     return state;
+    //   });
+    // }, 2000);
+    // setTimeout(() => {
+    //   this.setState(state => {
+    //     state.arr = [{
+    //       name: '-1'
+    //     }, ...state.arr];
+    //     return state;
+    //   });
+    // }, 3000);
+    // setTimeout(() => {
+    //   this.setState(state => {
+    //     state.preArr = [...state.preArr, {
+    //       name: '-2'
+    //     }];
+    //     return state;
+    //   });
+    // }, 4000);
+    // setTimeout(() => {
+    //   this.setState(state => {
+    //     state.ifyo = true;
+    //     return state;
+    //   });
+    // }, 5000);
+    // setTimeout(() => {
+    //   this.setState(state => {
+    //     state.subArr = [...state.subArr, {
+    //       name: 'e'
+    //     }];
+    //     return state;
+    //   });
+    // }, 6000);
+    // setTimeout(() => {
+    //   this.setState(state => {
+    //     state.arr = [];
+    //     return state;
+    //   });
+    // }, 7000);
+    // setTimeout(() => {
+    //   this.setState(state => {
+    //     return {
+    //       preArr: [],
+    //       arr: ['a', 'c'].map(item => ({
+    //         name: item
+    //       })),
+    //       ifyo: false,
+    //       subArr: [],
+    //       diff: '',
+    //     };
+    //   })
+    // }, 8000);
+    // setTimeout(() => {
+    //   console.dir(this.storeKeeper.store.toJS());
+    // }, 9000);
   }
 });
